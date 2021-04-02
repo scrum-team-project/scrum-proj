ea6a1d6be76cbcff39b65c173b6f9f9ae84d4ee9
Pawel Janowski <mpjanowski@wp.pl>
2021-04-02 14:04:00 +0200

    Poprawiono/Zmieniono model obywatela

---
+++ server/models/Citizen.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const citizenSchema = new Schema({
+    name: String,
+    surname: String,
+    gender: String,
+    dateOfBirth: Date,
+    matrialStatus: String,
+    education: String,
+    addres: String,
+    registeredAddress: String,
+    workplace: String,
+    worktype: String,
+    typeOfEmploymentContract: String,
+    earnings: Number,
+    spouse:  String, //ID,Pesel
+    kids: Number,
+    householdMembers: Number,
+    id: String,
+    nationality: String,
+    isDisabled: Boolean,
+});
+
+module.exports = model('Citizen', citizenSchema);
--- server/models/User.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const userSchema = new Schema({
-    login: String,
-    password: String,
-});
-
-module.exports = model('User', userSchema);
