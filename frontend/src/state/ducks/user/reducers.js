28f982fad02b58c806fa609474341b9570556e55
w.gulan <wojciech.gulan@gmail.com>
2021-04-03 19:30:33 +0200

    UI logowania, przygotowanie wysyłania requestu logowania na serwer

---
+++ frontend/src/state/ducks/user/reducers.js
@@ -0,0 +1,36 @@
+import types from "./types";
+
+const initialState = {
+    login: "",
+    password: "",
+    loggedIn: false,
+    message: ""
+}
+
+const loginReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.LOGIN_SUCCESS: {
+            return {
+                login: action.payload.login,
+                password: action.payload.password,
+                loggedIn: true
+            }
+        }
+        case types.LOGIN_FAILURE: {
+            return {
+                ...state,
+                message: "Niepoprawne dane logowania"
+            }
+        }
+
+        case types.LOGIN_REQUEST: {
+            console.log("eo")
+            return state
+        }
+
+        default:
+            return state
+    }
+}
+
+export default loginReducer;
