3d9dfe8a89aee5e1af7438491af3b316464e3e56
w.gulan <wojciech.gulan@gmail.com>
2021-04-03 19:34:18 +0200

    frontend fix

--- front/src/state/store/ducks/books/selectors.js
+++
@@ -1,7 +0,0 @@
-const allBooks = state => {
-  return state.entities.books.allIds.map(id => state.entities.books.byId[id]);
-};
-
-const selectors = { allBooks };
-
-export default selectors;
--- front/src/state/store/ducks/movies/selectors.js
+++
@@ -1,7 +0,0 @@
-const allMovies= state => {
-  return state.entities.movies.allIds.map(id => state.entities.movies.byId[id]);
-};
-
-const selectors = { allMovies };
-
-export default selectors;
