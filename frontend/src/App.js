aaeb3031b3003966214f4acee9988b3a4026096d
Dawid Dereszkiewicz <72603663+ddereszkiewicz@users.noreply.github.com>
2021-04-07 20:05:53 +0200

    asfsafsa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Summary from "./ui/Summary/Summary";
 function App() {
     return (
         <div className="App">
-            {/* <LoginForm/> */}
+            <LoginForm/>
             <Summary />
         </div>
     );
