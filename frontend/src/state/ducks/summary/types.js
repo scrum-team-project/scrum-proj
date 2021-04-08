3fdc12e0b1c52d33a56cb36794512317680bf347
Dawid Dereszkiewicz <72603663+ddereszkiewicz@users.noreply.github.com>
2021-04-08 11:31:42 +0200

    filter summary done on mock object ponieważ backend (Tymoteusz Bielski) nie zrobił endpointa

---
+++ frontend/src/state/ducks/summary/types.js
@@ -0,0 +1,5 @@
+export const FETCH_SUMMARY = "@summary/FETCH_SUMMARY";
+export const SUMMARY_REQUEST = "@@summary/SUMMARY_REQUEST";
+export const SUMMARY_SUCCESS = "@@summary/SUMMARY_SUCCESS";
+export const SUMMARY_FAILURE = "@@summary/SUMMARY_FAILURE";
+export const SELECT_TO_DISPLAY = "@@summary/SELECT_TO_DISPLAY";
