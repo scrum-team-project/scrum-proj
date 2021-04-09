7b8c9f447fffeaa2b7bd77f3ba53ee67a447d23f
Piotr Kira <piotrakira@gmail.com>
2021-04-09 08:40:05 +0200

    Add mock login

--- server/routes/Admin.js
+++ server/routes/Admin.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const Admin = require("../models/Admin");
+const { login } = require('../account');
 
 router.post("/login", async (req, res) => {
-
+  const username = req.body.username;
+  const password = req.body.password;
+  logged = login(username, password);
+  res.setHeader('Content-Type', 'application/json');
+  if (logged) {
+    res.end(JSON.stringify({sessionID: logged}))
+  }
+  res.end(JSON.stringify({error: 'Wrong password or username'}))
 });
 
 
