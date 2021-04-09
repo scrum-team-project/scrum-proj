8ebf8eefb072653b4ea3266e4b5e09c7a566976d
Piotr Kira <piotrakira@gmail.com>
2021-04-09 08:33:57 +0200

    Refactor

---
+++ server/routes/Admin.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const Admin = require("../models/Admin");
+
+router.post("/login", async (req, res) => {
+
+});
+
+
+module.exports = router;
