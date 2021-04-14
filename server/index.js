4b2554f9d582dceff2cbcfe18ac049731eb0fb78
Pawel Janowski <mpjanowski@wp.pl>
2021-04-14 15:53:39 +0200

    admi zmieniony

--- server/index.js
+++ server/index.js
@@ -22,6 +22,16 @@ const dbConnData = {
 };
 
 const mongoose = require("mongoose");
+ const addAdmin= async ()=>  {
+  try {
+    const adm=new Admin({login:"admin",password:"admin"})
+    await adm.save()
+    console.log('Dodano Admina');
+  } catch (error) {
+    console.log(error);
+  }}
+  
+
 
 mongoose
   .connect(
@@ -36,13 +46,12 @@ mongoose
     console.log(
       `Connected to MongoDB. Database name: "${response.connections[0].name}"`
     );
-    if(!Admin.exists({ login: 'admin' })){
-        const adm=new Admin({login:"admin",password:"admin"})
-        adm.save()
-    }
+    addAdmin()
     const port = process.env.PORT || 5000;
     app.listen(port, () => {
       console.log(`API server listening at http://localhost:${port}`);
     });
   })
   .catch((error) => console.error("Error connecting to MongoDB", error));
+
+ 
