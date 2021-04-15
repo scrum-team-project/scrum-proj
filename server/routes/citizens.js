509df1d34781ccd4d77485b523a5027fa8c9de5d
Tymoteusz Bielski <tymekb@onet.pl>
2021-04-15 23:37:14 +0200

    summaryByRegion endpoint

--- server/routes/citizens.js
+++ server/routes/citizens.js
@@ -19,21 +19,130 @@ router.get("/all", async (req, res) => {
   res.send(all);
 });
 
-router.get("/summary-population", async (req, res) => {
+router.get("/summaryByRegion", async (req, res) => {
   const all = await Citizen.find();
 
+  const worktypeNull = "";
+  const emptySummary = {
+    population: 0,
+    workingPopulation: 0,
+    workingPercentage: 0,
+  };
+
   const grouppedByRegion = all.reduce((acc, value) => {
-    if (!acc.find((el) => el.region == value.region)) {
+    if (!acc.find((el) => el.region === value.region)) {
       acc.push({
         region: value.region,
-        summary: {
-          population: 0,
-        },
+        districts: [
+          {
+            district: value.district,
+            communities: [
+              {
+                community: value.community,
+                cities: [
+                  { city: value.city, summaryCity: { ...emptySummary } },
+                ],
+                summaryCommunity: { ...emptySummary },
+              },
+            ],
+            summaryDistrict: { ...emptySummary },
+          },
+        ],
+        summaryRegion: { ...emptySummary },
       });
     }
 
-    let index = acc.findIndex((el) => el.region == value.region);
-    acc[index].summary.population += 1;
+    let indexRegion = acc.findIndex((el) => el.region === value.region);
+
+    if (
+      !acc[indexRegion].districts.find((el) => el.district === value.district)
+    ) {
+      acc[indexRegion].districts.push({
+        district: value.district,
+        communities: [
+          {
+            community: value.community,
+            cities: [{ city: value.city, summaryCity: { ...emptySummary } }],
+            summaryCommunity: { ...emptySummary },
+          },
+        ],
+        summaryDistrict: { ...emptySummary },
+      });
+    }
+
+    let indexDistrict = acc[indexRegion].districts.findIndex(
+      (el) => el.district === value.district
+    );
+
+    if (
+      !acc[indexRegion].districts[indexDistrict].communities.find(
+        (el) => el.community === value.community
+      )
+    ) {
+      acc[indexRegion].districts[indexDistrict].communities.push({
+        community: value.community,
+        cities: [{ city: value.city, summaryCity: { ...emptySummary } }],
+        summaryCommunity: { ...emptySummary },
+      });
+    }
+
+    let indexCommunity = acc[indexRegion].districts[
+      indexDistrict
+    ].communities.findIndex((el) => el.community === value.community);
+
+    if (
+      !acc[indexRegion].districts[indexDistrict].communities[
+        indexCommunity
+      ].cities.find((el) => el.city === value.city)
+    ) {
+      acc[indexRegion].districts[indexDistrict].communities[
+        indexCommunity
+      ].cities.push({ city: value.city, summaryCity: { ...emptySummary } });
+    }
+
+    let indexCity = acc[indexRegion].districts[indexDistrict].communities[
+      indexCommunity
+    ].cities.findIndex((el) => el.city === value.city);
+
+    const summaryRegion = acc[indexRegion].summaryRegion;
+    const summaryDistrict =
+      acc[indexRegion].districts[indexDistrict].summaryDistrict;
+    const summaryCommunity =
+      acc[indexRegion].districts[indexDistrict].communities[indexCommunity]
+        .summaryCommunity;
+    const summaryCity =
+      acc[indexRegion].districts[indexDistrict].communities[indexCommunity]
+        .cities[indexCity].summaryCity;
+
+    summaryRegion.population += 1;
+    summaryDistrict.population += 1;
+    summaryCommunity.population += 1;
+    summaryCity.population += 1;
+
+    if (!value.worktype == "") {
+      summaryRegion.workingPopulation += 1;
+      summaryDistrict.workingPopulation += 1;
+      summaryCommunity.workingPopulation += 1;
+      summaryCity.workingPopulation += 1;
+    }
+
+    summaryRegion.workingPercentage =
+      Math.round(
+        (summaryRegion.workingPopulation / summaryRegion.population) * 10000
+      ) / 100;
+    summaryDistrict.workingPercentage =
+      Math.round(
+        (summaryDistrict.workingPopulation / summaryDistrict.population) * 10000
+      ) / 100;
+    summaryCommunity.workingPercentage =
+      Math.round(
+        (summaryCommunity.workingPopulation / summaryCommunity.population) *
+          10000
+      ) / 100;
+    summaryCity.workingPercentage =
+      Math.round(
+        (summaryCity.workingPopulation / summaryCity.population) * 10000
+      ) / 100;
 
     return acc;
   }, []);
@@ -56,13 +165,17 @@ router.patch("/:id", async (req, res) => {
   const id = req.params.id;
   const updated = req.body;
 
-  await Citizen.findOneAndUpdate({ _id: id }, { $set: updated }, (error, updated) => {
+  await Citizen.findOneAndUpdate(
+    { _id: id },
+    { $set: updated },
+    (error, updated) => {
       if (error) {
         res.send(false);
       } else {
         res.send(true);
       }
-    });
+    }
+  );
 });
 
 module.exports = router;
