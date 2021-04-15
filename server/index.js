9752f41c371ed78ae9917ef61ce8e4d0e0420fa4
Tymoteusz Bielski <72604436+tbielski@users.noreply.github.com>
2021-04-16 00:02:52 +0200

    groupByRegion endpoint (#32)
    * refactoring backend + summary-population endpoint
    
    * summaryByRegion endpoint

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ app.use("/citizens", citizens);
 app.use("/accounts", accounts);
 app.use("/pollsters", pollsters);
 
-
 app.post("/ping", (req, res) => {
   res.send("pong");
 });
@@ -28,30 +27,34 @@ const dbConnData = {
 };
 
 const mongoose = require("mongoose");
- 
-  const addAdmin= async ()=>  {
-    try {
-      if(await !Admin.exists({ login: 'admin' })){
-      const adm=new Admin({login:"admin",password:"admin"})
-      await adm.save()
-      console.log('Dodano Admina !!!');
-      }
-    } catch (error) {
-      console.log(error);
-    }}
-     
-  const addUser= async ()=>  {
-    try {
-      if(await !User.exists({ email: 'user@example.com' }))
-      {
-      const user=new User({ email: 'user@example.com',password:"password"})
-      await user.save()
-      console.log('Dodano Usera !!!');
-      }
+
+const addAdmin = async () => {
+  try {
+    if (await !Admin.exists({ login: "admin" })) {
+      const adm = new Admin({ login: "admin", password: "admin" });
+      await adm.save();
+      console.log("Dodano Admina !!!");
     }
-    catch (error) {
-      console.log(error);
-    }}
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addUser = async () => {
+  try {
+    if (await !User.exists({ email: "user@example.com" })) {
+      const user = new User({
+        email: "user@example.com",
+        password: "password",
+      });
+      await user.save();
+      console.log("Dodano Usera !!!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 mongoose
   .connect(
     `mongodb://${dbConnData.host}:${dbConnData.port}/${dbConnData.database}`,
@@ -65,13 +68,12 @@ mongoose
     console.log(
       `Connected to MongoDB. Database name: "${response.connections[0].name}"`
     );
-    addAdmin()
-    addUser()
+    addAdmin();
+    addUser();
+
     const port = process.env.PORT || 5000;
     app.listen(port, () => {
       console.log(`API server listening at http://localhost:${port}`);
     });
   })
   .catch((error) => console.error("Error connecting to MongoDB", error));
-
-
