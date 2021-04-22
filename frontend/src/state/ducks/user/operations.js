8f5ec47065c5bb431a49284b63656636784ec155
Piotr Kira <piotrakira@gmail.com>
2021-04-22 18:06:19 +0200

    Add admin login

--- frontend/src/state/ducks/user/operations.js
+++ frontend/src/state/ducks/user/operations.js
@@ -1,11 +1,26 @@
 import { createAction } from "redux-api-middleware";
 import types from "./types";
 
-const login = (login, password) => (dispatch) => {
-    console.log("LOGIN REQUEST");
+const login = (login, password, isAdmin) => (dispatch) => {
+    if(isAdmin) {
+        dispatch(
+            createAction({
+                endpoint: "http://localhost:5000/accounts/login-admin",
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ login, password }),
+                types: [
+                    types.LOGIN_REQUEST,
+                    types.LOGIN_ADMIN_SUCCESS,
+                    types.LOGIN_FAILURE,
+                ],
+            })
+        );
+        return;
+    }
     dispatch(
         createAction({
-            endpoint: "http://localhost:5000/accounts/login",
+            endpoint: "http://localhost:5000/accounts/login-user",
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ login, password }),
